78c393d829ca6629209410370bd986ef03a90a09
CB6 <melissa.j.womack@gmail.com>
2018-08-29 10:53:39 -0500

    Added node package variables, added routes to html files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+app.get("/", function(req, res){
+    res.sendFile(path.join(_dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(_dirname, "survey.html"));
+});
