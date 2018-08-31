77b27b71e0dc2ab8493c4e1388d9fd6727fe58b1
CB6 <melissa.j.womack@gmail.com>
2018-08-31 15:54:26 -0500

    Fixed grammer in survey, changed friendsData

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,27 @@ var friendsData =
         {
             name: "Justin Timberlake",
             photo: "http://www.gstatic.com/tv/thumb/persons/170201/170201_v9_bb.jpg",
-            scores: [2, 5, 1, 2, 1, 2, 1, 1, 5, 3]
+            scores: [4, 5, 1, 1, 1, 1, 1, 1, 5, 1]
         },
         {
             name: "Joey Fatone",
             photo: "http://www.gstatic.com/tv/thumb/persons/207727/207727_v9_ba.jpg",
-            scores: [1, 2, 2, 3, 1, 1, 4, 5, 1, 2]
+            scores: [4, 1, 2, 1, 1, 1, 4, 5, 1, 1]
         },
         {
             name: "Lance Bass",
             photo: "http://www.gstatic.com/tv/thumb/persons/207726/207726_v9_bb.jpg",
-            scores: [5, 3, 1, 1, 1, 5, 1, 1, 1, 3]
+            scores: [5, 4, 1, 1, 1, 5, 1, 1, 1, 1]
         },
         {
             name: "JC Chasez",
             photo: "http://www.gstatic.com/tv/thumb/persons/207725/207725_v9_ba.jpg",
-            scores: [2, 2, 2, 2, 1, 2, 1, 1, 1, 5]
+            scores: [4, 1, 1, 1, 1, 5, 1, 1, 1, 5]
         },
         {
             name: "Chris Kirkpatrick",
             photo: "http://www.gstatic.com/tv/thumb/persons/207728/207728_v9_ba.jpg",
-            scores: [5, 2, 5, 5, 5, 1, 5, 3, 1, 1]
+            scores: [5, 1, 5, 5, 5, 1, 5, 4, 1, 1]
         },
         
     ]
