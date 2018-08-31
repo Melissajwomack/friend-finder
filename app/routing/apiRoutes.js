d1b6ef39d2bcde690fad39b02b4165a569dd32e5
CB6 <melissa.j.womack@gmail.com>
2018-08-30 19:22:15 -0500

    Added routes to server.js, added listen function, created apiroutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,14 @@
+var friendsData = require("../data/friends");
+
+
+
 function apiRoutes(app, connection) {
     app.get("/api/friends", function (req, res) {
-        res.sendFile(path.join(_dirname, "home.html"));
+        res.json(friendsData);
     });
 
-    app.post("/api/friends", function (req, res) {
-        res.sendFile(path.join(_dirname, "survey.html"));
+    app.post("/api/friends", function(req, res) {
+        friendsData.push(req.body);
     });
 }
 
