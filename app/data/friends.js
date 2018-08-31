8b77b07a9d96dc7f62cd1f09b845bbde2619ba48
CB6 <melissa.j.womack@gmail.com>
2018-08-31 12:45:22 -0500

    Updated friends data and survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,32 @@
-var friendsData = 
-[ 
-    {
-    name: "Melissa",
-    photo: "https://avatars0.githubusercontent.com/u/37938487?s=460&v=4",
-    scores: [5,3,2,1,3,4,5,3,2,1]
-    },
-]
+var friendsData =
+    [
+        {
+            name: "Justin Timberlake",
+            photo: "https://avatars0.githubusercontent.com/u/37938487?s=460&v=4",
+            scores: [2, 5, 1, 2, 1, 2, 1, 1, 5, 3]
+        },
+        {
+            name: "Joey Fatone",
+            photo: "https://avatars0.githubusercontent.com/u/37938487?s=460&v=4",
+            scores: [1, 2, 2, 3, 1, 1, 4, 5, 1, 2]
+        },
+        {
+            name: "Lance Bass",
+            photo: "https://avatars0.githubusercontent.com/u/37938487?s=460&v=4",
+            scores: [5, 3, 1, 1, 1, 5, 1, 1, 1, 3]
+        },
+        {
+            name: "JC Chasez",
+            photo: "https://avatars0.githubusercontent.com/u/37938487?s=460&v=4",
+            scores: [2, 2, 2, 2, 1, 2, 1, 1, 1, 5]
+        },
+        {
+            name: "Chris Kirkpatrick",
+            photo: "https://avatars0.githubusercontent.com/u/37938487?s=460&v=4",
+            scores: [5, 2, 5, 5, 5, 1, 5, 3, 1, 1]
+        },
+        
+    ]
 
 module.exports = friendsData;
 
